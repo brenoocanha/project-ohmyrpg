f890e33925d6c62af9beda7475f77d2bdfb1cee5
Breno Ocanha <breno@dmxdesign.com.br>
2023-10-12 16:28:35 -0300

    first commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+
+
+export function Header() {
+  return(
+    <header className="bg-gradient-to-br from-[#282828] to-[#1C1C1C] h-20">
+      <div className="container w-full">
+
+      </div>
+    </header>
+  )
+}
